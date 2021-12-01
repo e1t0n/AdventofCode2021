7d2b5ede131d9344f5efa69647e34fd72eb0d496
Elton Livera <elton.livera@natwest.com>
2021-12-01 14:06:02 +0000

    Day1

---
+++ MyPlayground.playground/Pages/01.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+
+let file = Bundle.main.url(forResource: "input", withExtension: "txt")!
+let content = try! String(contentsOf: file, encoding: .utf8)
+let depths = content.split(whereSeparator: \.isNewline).compactMap { Int($0) }
+
+func part1(_ m: [Int]) -> Int {
+    var increased = 0
+    for (index, element) in m.enumerated() {
+        if index+1 < m.count {
+            if element < m[index+1] {                
+                increased += 1
+            }
+        }
+    }
+    return increased
+}
+
+func part2() -> [Int] {
+    var array = depths
+    var measurements = [Int]()
+    while array.count > 2 {
+        measurements.append(array.prefix(3).reduce(0, +))
+        array.removeFirst()
+    }
+    return measurements
+}
+
+print(part1(depths))
+print(part1(part2()))
+
